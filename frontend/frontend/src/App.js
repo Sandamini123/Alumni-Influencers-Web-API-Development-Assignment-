f6b19b09e340f8bdff3e4cc2df0efcf0d53c1d81
Sandamini Bandara <sandamini.bandara@lk.onsystechnologies.com>
2026-03-07 19:14:22 +0530

    Frontend - register user part created

---
+++ frontend/frontend/src/App.js
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import VerifyEmail from "./pages/VerifyEmail";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/verify-email" element={<VerifyEmail />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
